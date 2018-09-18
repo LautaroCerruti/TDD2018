0725304d7383a8ff066e5d3464ff6f28da75e4d8
LautaroCerruti <39503825+LautaroCerruti@users.noreply.github.com>
2018-09-18 17:19:08 -0300

    Add files via upload

---
+++ src/Carta.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TDD;
+
+class Carta {
+  protected $tipo;
+  protected $palo;
+  protected $numero;
+  public function __construct($tipo,$palo,$numero){
+	$this->tipo= $tipo;
+	$this->palo= $palo;
+	$this->numero= $numero;
+  }		
+
+
+  public function palo() {
+	return $this->palo();
+  }
+
+  public function tipo() {
+	return $this->tipo;
+}
+
+  public function numero() {
+	return $this->numero;
+}
+}
