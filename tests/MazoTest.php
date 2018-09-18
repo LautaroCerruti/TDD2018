bdb1ddb51c744e07a2f10b9ebd45e9f83aa9522a
LautaroCerruti <39503825+LautaroCerruti@users.noreply.github.com>
2018-09-18 17:48:20 -0300

    Test Funciones mazo

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -10,14 +10,34 @@ class MazoTest extends TestCase {
      * Valida que se puedan crear mazos de cartas.
      */
     public function testExiste() {
-        $mazo = new Mazo;
+        $mazo = new Mazo("Español",50);
         $this->assertTrue(isset($mazo));
     }
 
     public function testMezclable() {
-        $mazo = new Mazo;
-        $this->assertTrue($mazo->mezclar());
+        $mazo = new Mazo("Poker",50);
+	$copia = $mazo;
+	$mazo->mezclar();
+        $this->assertFalse($mazo==$copia);
     }
 
+    public function testCortar() {
+	$mazo= new Mazo("Español",50);
+	$copia = $mazo;
+	$mazo->cortar();
+        $this->assertFalse($mazo==$copia);
+}
 
+    public function testContarCartas() {
+	$mazo= new Mazo("Español",50);
+        $this->assertEquals($mazo->ConCart(),50);
+}
+    public function testObtenerCarta(){
+	$mazo= new Mazo("Español",50);
+	$carta= new Carta("Español","Oro",1);
+        $this->assertEquals($mazo->ObtenerCarta(),);
+}
+	
+
+    
 }
