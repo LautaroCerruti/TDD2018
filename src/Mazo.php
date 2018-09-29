e2088f7b926abc0f098e0fd247151e0080cbbed2
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-09-29 20:41:56 -0300

    Construct y mezclar

--- src/Mazo.php
+++ src/Mazo.php
@@ -4,9 +4,85 @@ namespace TDD;
 
 class Mazo
 {
+    private $cartas = array();
+
+    private $cant_cart = 0;
+
+    public function __construct($tipo)
+    {
+        switch ($tipo) {
+            case "Español":
+                for ($i = 1; $i < 13; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Española", "Oro", $i);
+                    $this->cant_cart++;
+                }
+                for ($i = 1; $i < 13; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Española", "Copa", $i);
+                    $this->cant_cart++;
+                }
+                for ($i = 1; $i < 13; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Española", "Basto", $i);
+                    $this->cant_cart++;
+                }
+                for ($i = 1; $i < 13; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Española", "Espada", $i);
+                    $this->cant_cart++;
+                }
+                $this->cartas[$this->cant_cart] = new Carta("Española", "Joker", 1);
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Española", "Joker", 2);
+                $this->cant_cart++;
+                break;
+            case "Poker":
+                for ($i = 1; $i < 11; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Poker", "Diamante", $i);
+                    $this->cant_cart++;
+                }
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Diamante", "J");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Diamante", "Q");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Diamante", "K");
+                $this->cant_cart++;
+                for ($i = 1; $i < 11; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Poker", "Corazon", $i);
+                    $this->cant_cart++;
+                }
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Corazon", "J");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Corazon", "Q");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Corazon", "K");
+                $this->cant_cart++;
+                for ($i = 1; $i < 11; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Poker", "Trebol", $i);
+                    $this->cant_cart++;
+                }
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Trebol", "J");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Trebol", "Q");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Trebol", "K");
+                $this->cant_cart++;
+                for ($i = 1; $i < 11; $i++) {
+                    $this->cartas[$this->cant_cart] = new Carta("Poker", "Pica", $i);
+                    $this->cant_cart++;
+                }
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Pica", "J");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Pica", "Q");
+                $this->cant_cart++;
+                $this->cartas[$this->cant_cart] = new Carta("Poker", "Pica", "K");
+                $this->cant_cart++;
+                break;
+            default:break;
+        }
+    }
 
     public function mezclar()
     {
+        if (!count($this->cartas)) {return false;}
+        shuffle($this->cartas);
         return true;
     }
 }
--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -10,37 +10,37 @@ class MazoTest extends TestCase {
      * Valida que se puedan crear mazos de cartas.
      */
     public function testExiste() {
-        $mazo = new Mazo("Español",50);
+        $mazo = new Mazo("Español");
         $this->assertTrue(isset($mazo));
     }
 
     public function testMezclable() {
-        $mazo = new Mazo("Poker",50);
+        $mazo = new Mazo("Poker");
 	    $copia = $mazo;
 	    $mazo->mezclar();
         $this->assertFalse($mazo==$copia);
     }
 
     public function testCortar() {
-	    $mazo= new Mazo("Español",50);
+	    $mazo= new Mazo("Español");
 	    $copia = $mazo;
 	    $mazo->cortar();
         $this->assertFalse($mazo==$copia);
 }
 
     public function testContarCartas() {
-	    $mazo= new Mazo("Español",50);
+	    $mazo= new Mazo("Español");
         $this->assertEquals($mazo->ConCart(),50);
 }
     public function testObtenerCarta(){
-	    $mazo= new Mazo("Español",50);
+	    $mazo= new Mazo("Español");
 	    $carta= $mazo->ObtenerCarta();
         $this->assertEquals($carta->palo(),"Oro");
         $this->assertEquals($carta->tipo(),"Español");
         $this->assertEquals($carta->numero(),1);
 }
     public function testIndicarSiTiene(){
-        $mazo = new Mazo("Español",50);
+        $mazo = new Mazo("Español");
         $this->assertEquals($mazo->TieneCartas(),true);
         for($i=0,$i<50,$i++){
             $mazo->ObtenerCarta();
@@ -49,7 +49,7 @@ class MazoTest extends TestCase {
     }
     
     public function testAgregarCarta(){
-        $mazo = new Mazo("Español",50);
+        $mazo = new Mazo("Español");
         $carta = new Carta("Española", "Copa", 4);
         $mazo->Agregar($carta);
         $this->assertEquals($mazo->ConCart(),51);
